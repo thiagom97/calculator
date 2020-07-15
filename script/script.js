931ff6548b95081c963574f40eb3696e3a73d095
Thiago <thiagommilitao@gmail.com>
2020-07-15 19:41:31 -0300

    Started adding JS functions

--- script/script.js
+++ script/script.js
@@ -26,4 +26,30 @@ function operate(num1, operator, num2) {
     }
 
 }
-console.log(operate(divide(), 16, 8))
+displayText = document.querySelector("#displayText");
+let numbers = document.querySelector(".numbers");
+let numberClick = numbers.querySelectorAll("button");
+numberClick.forEach((button) => {
+    button.addEventListener("click", function () {
+        const dotIndex = displayText.innerHTML.indexOf(".");
+        if ((dotIndex > -1)) {
+            document.getElementById("dot").disabled = true;
+        }
+        displayText.innerText += button.innerText;
+
+    })
+})
+//Delete 1 character
+let del = document.getElementById("del");
+del.addEventListener("click", function(){    
+    let deletedNumber = displayText.innerText;
+    displayText.innerText = deletedNumber.substring(0, deletedNumber.length -1);
+    
+})
+
+//Clear Display
+let clear = document.getElementById("clear")
+clear.addEventListener("click", function(){
+    let span = document.querySelector("span");
+    span.innerText = "";
+})
